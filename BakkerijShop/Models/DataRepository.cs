7d375544965c725f398876adc87d1d9774ecb48f
steven <nonkelchameau@gmail.com>
2018-12-15 16:37:21 +0100

    Aan 6 voorlopig.Saving adding modifying delete data.

--- BakkerijShop/Models/DataRepository.cs
+++ BakkerijShop/Models/DataRepository.cs
@@ -7,17 +7,53 @@ namespace BakkerijShop.Models
 {
     public class DataRepository : IRepository
     {
-        //private List<Product> data = new List<Product>(); nu data-acces via context
         private DataContext context;
 
         public DataRepository(DataContext ctx) => context = ctx;
 
         public IEnumerable<Product> Products => context.Products.ToArray();
 
+        public Product GetProduct(long key) => context.Products.Find(key);
+
         public void AddProduct(Product product)
         {
-            this.context.Add(product);
-            this.context.SaveChanges();
+            context.Products.Add(product);
+            context.SaveChanges();
         }
+
+        public void UpdateProduct(Product product)
+        {
+            Product p = GetProduct(product.Id);
+            p.Naam = product.Naam;
+            p.Categorie = product.Categorie;
+            p.Prijs = product.Prijs;
+            //context.Products.Update(product);
+            context.SaveChanges();
+        }
+
+        public void UpdateAll(Product[] products)
+        {
+            //context.Products.UpdateRange(products);
+
+            Dictionary<long, Product> data = products.ToDictionary(p => p.Id);
+            IEnumerable<Product> baseline =
+                context.Products.Where(p => data.Keys.Contains(p.Id));
+
+            foreach (Product databaseProduct in baseline)
+            {
+                Product requestProduct = data[databaseProduct.Id];
+                databaseProduct.Naam = requestProduct.Naam;
+                databaseProduct.Categorie = requestProduct.Categorie;
+                databaseProduct.Prijs = requestProduct.Prijs;
+            }
+            context.SaveChanges();
+        }
+
+        public void Delete(Product product)
+        {
+            context.Products.Remove(product);
+            context.SaveChanges();
+        }
+
     }
 }
